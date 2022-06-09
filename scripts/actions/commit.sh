8ad7a46ffd69abc5c201a6c0f5d3855e1d30a391
Douglas Martins <douglas.martins.m099@gmail.com>
2022-06-08 22:38:42 -0300

    Enhance Debug

--- scripts/actions/commit.sh
+++ scripts/actions/commit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo ""
 echo $(date)
 git add .git-preserve-permissions
 git add $(realpath --relative-to="$BACKUP_ROOT" "$1")
--- scripts/actions/cron.sh
+++ scripts/actions/cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$(date) - Backing up: $SCHED_PATH" >> /var/log/cron.log 2>&1
+echo "$(date) - Backing up: $SCHED_PATH$1" >> /var/log/cron.log 2>&1
 /workspace/scripts/actions/copy.sh "$SCHED_PATH" "$SCHED_PATH$1"
 /workspace/scripts/actions/commit.sh "$SCHED_PATH$1" "[$1] SCHEDULED"
