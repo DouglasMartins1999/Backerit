b9ca9ffb24d67d2da21c5659d5a23bfef4ea5274
Your Name <you@example.com>
2022-06-09 17:28:43 -0300

    Finish Backerit

--- scripts/setup/repo.sh
+++ scripts/setup/repo.sh
@@ -9,9 +9,17 @@ if [ $(git remote | wc -l) -eq 0 ]; then
     git config --global credential.helper store
     git config --global user.email "$GIT_EMAIL"
     git config --global user.name "$GIT_USERNAME"
+
+    git config --global preserve-permissions.user true
+    git config --global preserve-permissions.group true
     
     git remote add origin $GIT_REMOTE_URL
     git pull --set-upstream origin $(git symbolic-ref --short HEAD)
     
     /workspace/scripts/sync.sh
+fi
+
+if [ ! -e "$GIT_WORK_TREE/.git-preserve-permissions" ]; then
+    git preserve-permissions --save
+    /workspace/scripts/actions/commit.sh "$BACKUP_ROOT" "PERMISSIONS' FILE"
 fi
